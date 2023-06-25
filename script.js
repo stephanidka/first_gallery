04f75357239f673aea6a44c5df4ae0529e8a0ed1
stephanidka <stephany.vakhotina@gmail.com>
2023-06-25 23:32:14 +0200

    gallery

---
+++ script.js
@@ -0,0 +1,10 @@
+const pic1 = document.getElementById('pic1');
+console.log(pic1);
+
+function addImage2(){
+    pic1.src = 'maneki2.jpg';
+};
+
+function addImage1(){
+    pic1.src = 'maneki1.jpg';
+};
